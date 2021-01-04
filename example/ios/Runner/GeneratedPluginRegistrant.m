e22b6e61b78b977dfd0ccfa9328ce765b116e53a
Daniele D'Agostino <principelele@gmail.com>
2021-01-04 22:33:11 +0100

    first commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,47 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<integration_test/IntegrationTestPlugin.h>)
+#import <integration_test/IntegrationTestPlugin.h>
+#else
+@import integration_test;
+#endif
+
+#if __has_include(<espresso/EspressoPlugin.h>)
+#import <espresso/EspressoPlugin.h>
+#else
+@import espresso;
+#endif
+
+#if __has_include(<flutter_local_notifications/FlutterLocalNotificationsPlugin.h>)
+#import <flutter_local_notifications/FlutterLocalNotificationsPlugin.h>
+#else
+@import flutter_local_notifications;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
+  [EspressoPlugin registerWithRegistrar:[registry registrarForPlugin:@"EspressoPlugin"]];
+  [FlutterLocalNotificationsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterLocalNotificationsPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
---
+++ example/lib/notify.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class Notify {
+  FlutterLocalNotificationsPlugin plugin = FlutterLocalNotificationsPlugin();
+  AndroidInitializationSettings androidSettings;
+  IOSInitializationSettings iosSetting;
+  InitializationSettings initSettings;
+
+  Notify() {
+    init();
+  }
+
+  void init() async {
+    androidSettings = AndroidInitializationSettings('app_icon');
+    iosSetting = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    initSettings =
+        InitializationSettings(android: androidSettings, iOS: iosSetting);
+    await plugin.initialize(initSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    return CupertinoAlertDialog(
+      title: Text(title),
+      content: Text(body),
+      actions: <Widget>[
+        CupertinoDialogAction(
+            isDefaultAction: true, onPressed: () {}, child: Text('OK'))
+      ],
+    );
+  }
+
+  Future onSelectNotification(String payload) {
+    //navigator
+  }
+
+  static Future<void> setAlarm() async {
+    //await AndroidAlarmManager.periodic(
+    //  const Duration(seconds: 20), formAlarm, _show);
+
+    Notify notify = Notify();
+    notify.show();
+  }
+
+  Future<void> show() async {
+    await notifications('Fill form', 'Please rember to fill and sent the form');
+  }
+
+  Future<void> notifications(String title, String body) async {
+    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+        'channelId', 'channelName', 'channelDescription',
+        priority: Priority.high, importance: Importance.max, ticker: 'Test');
+    IOSNotificationDetails iosDetail = IOSNotificationDetails();
+    NotificationDetails details =
+        NotificationDetails(android: androidDetails, iOS: iosDetail);
+    await plugin.show(0, title, body, details);
+  }
+}
